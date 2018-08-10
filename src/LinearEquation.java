cadfabe99d8c87617eed3705d15f6ffb209866ad
Tien Dat <datdta95@gmail.com>
2018-08-10 15:07:34 +0700

    Nhập vào hệ số của phương trình

---
+++ src/LinearEquation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LinearEquation {
+    public static void main(String[] args) {
+        //Nhập vào hệ số của phương trình
+        float a;
+        float b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\tGIẢI PHƯƠNG TRÌNH BẬC NHẤT  ax + b = 0");
+        System.out.println("Nhập vào hệ số a: ");
+        a = scanner.nextFloat();
+        System.out.println("Nhập vào hệ số b: ");
+        b = scanner.nextFloat();
+    }
+}
